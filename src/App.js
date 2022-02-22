979d11932b244ba9c1875bfae4b83d057893a7db
Alan Richards <ajr.pp.ie@gmail.com>
2022-02-22 14:22:49 +0000

    Updated default feedback state

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 	const blockchain = useSelector((state) => state.blockchain);
 	const data = useSelector((state) => state.data);
 	const [claimingNft, setClaimingNft] = useState(false);
-	const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
+	const [feedback, setFeedback] = useState(`Click below to mint your NFT.`);
 	const [mintAmount, setMintAmount] = useState(1);
 	const [CONFIG, SET_CONFIG] = useState({
 		CONTRACT_ADDRESS: "",
